63c44a73673615667fe596860e2426f6ba24a974
Alexandr1st <127955641+Alexandr1st@users.noreply.github.com>
2025-08-08 17:30:06 +0300

    Create AuthContext.jsx

---
+++ src/state/AuthContext.jsx
@@ -0,0 +1,44 @@
+// Создаем провайдер
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  // Здесь может быть логика для проверки токена в localStorage при загрузке
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setToken(storedToken);
+      setIsLoggedIn(true);
+      // Возможно, здесь вы будете получать данные пользователя по токену
+    }
+  }, []);
+
+  const login = (userData, userToken) => {
+    setUser(userData);
+    setToken(userToken);
+    setIsLoggedIn(true);
+    localStorage.setItem('token', userToken);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    setIsLoggedIn(false);
+    localStorage.removeItem('token');
+  };
+
+  const value = {
+    isLoggedIn,
+    user,
+    token,
+    login,
+    logout
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
