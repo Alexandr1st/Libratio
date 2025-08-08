d4a08d77f8175b323cd71a6ed719bbe94b07dc20
Alexandr1st <127955641+Alexandr1st@users.noreply.github.com>
2025-08-08 14:43:10 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: '#2563eb'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
